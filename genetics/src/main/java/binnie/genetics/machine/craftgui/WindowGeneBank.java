0f73bd4da9e824b74a4e4e2c862acc2f4e511c1d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-03-08 22:17:28 +0100

    Fix inadvertent division in texts

--- genetics/src/main/java/binnie/genetics/machine/craftgui/WindowGeneBank.java
+++ genetics/src/main/java/binnie/genetics/machine/craftgui/WindowGeneBank.java
@@ -124,7 +124,7 @@ public class WindowGeneBank extends WindowMachine {
 			}
 		}
 		new ControlText(panelProject, new Point(4, 4), "§nFull Genome Project");
-		new ControlText(panelProject, new Point(4, 18), "§oSequenced §r" + seqGenes + '/' + totalGenes + " §oGenes");
+		new ControlText(panelProject, new Point(4, 18), "§oSequenced §r" + seqGenes + "/" + totalGenes + " §oGenes");
 	}
 
 	@Override
@@ -192,7 +192,7 @@ public class WindowGeneBank extends WindowMachine {
 						}
 					}
 				}
-				tooltip.add(seqGenes + '/' + totalGenes + " Genes");
+				tooltip.add(seqGenes + "/" + totalGenes + " Genes");
 			}
 		}
 	}
