087c18c20ea487abb1312061f8a803720d1a640c
Cat In Minecraft <minecraftcat@foxmail.com>
2026-06-16 14:55:21 +0800

    Localize temperature display data

--- botany/src/main/java/binnie/botany/gui/database/PageSpeciesFlowerGenome.java
+++ botany/src/main/java/binnie/botany/gui/database/PageSpeciesFlowerGenome.java
@@ -53,7 +53,7 @@ public class PageSpeciesFlowerGenome extends PageSpecies {
 		int th = 14;
 		int th2 = 18;
 		new ControlText(contents, new Area(0, y, w2, th), I18N.localise(DatabaseConstants.BOTANY_GENOME_KEY + ".temp"), TextJustification.MIDDLE_RIGHT);
-		new ControlText(contents, new Area(w2, y, w3, th), flowerSpecies.getTemperature().getName(), TextJustification.MIDDLE_LEFT);
+		new ControlText(contents, new Area(w2, y, w3, th), AlleleHelper.toDisplay(flowerSpecies.getTemperature()), TextJustification.MIDDLE_LEFT);
 
 		y += th;
 		new ControlText(contents, new Area(0, y, w2, th), I18N.localise(DatabaseConstants.BOTANY_GENOME_KEY + ".moist"), TextJustification.MIDDLE_RIGHT);
