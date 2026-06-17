28df55f1a37b0fd6c6eaa3c6ab561a9c8bce4774
Cat In Minecraft <minecraftcat@foxmail.com>
2026-06-17 10:33:30 +0800

    Unify localization key names

--- design/src/main/java/binnie/design/blocks/BlockDesign.java
+++ design/src/main/java/binnie/design/blocks/BlockDesign.java
@@ -148,7 +148,7 @@ public abstract class BlockDesign extends BlockMetadata implements IMultipassBlo
 	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
 		DesignBlock block = DesignHelper.getDesignBlock(this.getDesignSystem(), TileEntityMetadata.getItemDamage(stack));
 		if (block.getPrimaryMaterial() != block.getSecondaryMaterial()) {
-			tooltip.add(I18N.localise("design.pattern.and", block.getPrimaryMaterial().getDesignMaterialName(), block.getSecondaryMaterial().getDesignMaterialName()));
+			tooltip.add(I18N.localise("binniedesign.pattern.and", block.getPrimaryMaterial().getDesignMaterialName(), block.getSecondaryMaterial().getDesignMaterialName()));
 		} else {
 			tooltip.add(block.getPrimaryMaterial().getDesignMaterialName());
 		}
